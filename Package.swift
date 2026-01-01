7826392822bd62684bcb203667783c8198176c05
rgmez <rob.gomez.mu@gmail.com>
2026-01-01 19:01:37 +0100

    bump minimum deployment targets to iOS 17/macOS 14/tvOS 17/watchOS 10

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "ViewStateKit",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8)
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10)
     ],
     products: [
         .library(name: "ViewStateKit", targets: ["ViewStateKit"])
